5ae3266a4f658bc83b04aef51c83d8691a4aaa34
muh-teguh <muhteguhadhiputra15@gmail.com>
2020-10-12 12:49:59 +0800

    tugas-11

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Tugas10 from './Tugas-10/Tugas10';
+import Tugas11 from './Tugas-11/Tugas11';
 import Tugas9 from './Tugas-9/Tugas9';
 
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Tugas9 />
       <Tugas10 />
+      <Tugas11 />
     </div >
   );
 }
---
+++ src/Tugas-11/Tugas11.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import './Tugas11.css'
+
+export default class Tugas11 extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 10,
+            time: new Date().toLocaleTimeString(),
+            show: true
+        }
+    }
+    componentDidMount() {
+        if (this.props.start !== undefined) {
+            this.setState({ count: this.props.start })
+        }
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.count === 0) {
+            this.setState({ show: false })
+        }
+    }
+
+    tick() {
+        this.setState({
+            count: this.state.count - 1,
+            time: new Date().toLocaleTimeString()
+        });
+    }
+    render() {
+        return this.state.show && (<Content time={this.state.time} count={this.state.count} timerID={this.timerID} />)
+    }
+}
+
+class Content extends Component {
+
+    componentWillUnmount() {
+        console.log('ngilang')
+        clearInterval(this.props.timerID);
+    }
+    render() {
+        return (
+            <div className="tugas11-container">
+                <h1>Sekarang Jam : {this.props.time}</h1>
+                <h1>Hitung Mundur : {this.props.count}</h1>
+            </div>
+        )
+    }
+}
